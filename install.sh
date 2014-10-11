d1130f8678e8762fdd0de483df58734ed033c3eb
abs <abs.pathak@ticketmaster.com>
2014-10-10 22:38:47 -0700

    Added logging

--- install.sh
+++ install.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
+NAME="serf"
 SERF_PKG_NAME="0.6.3_linux_amd64.zip"
 SERF_PKG_URL="https://dl.bintray.com/mitchellh/serf/$SERF_PKG_NAME"
-SERF_CFG_DIR="/etc/serf"
+SERF_CFG_DIR="/etc/$NAME"
 SERF_INSTALL_DIR="/usr/local/bin"
 
 
 startup_script() {
-	cp -v etc/init.d/serf /etc/init.d/
-	cp -v etc/sysconfig/serf /etc/sysconfig/
+	echo " * Installing startup scripts..."
+	cp etc/init.d/$NAME /etc/init.d/
+	cp etc/sysconfig/$NAME /etc/sysconfig/
 }
 
 setup_dirs() {
+	echo " * Creating config directory..."
 	[ -e "$SERF_CFG_DIR" ] || mkdir $SERF_CFG_DIR
-	rsync -vaH etc/serf/ $SERF_CFG_DIR
+	rsync -aH etc/$NAME/ $SERF_CFG_DIR
 }
 
 fetch_install_serf_bin() {
-	[ -e "$SERF_INSTALL_DIR" ] || {
+	if [ -e "$SERF_INSTALL_DIR/$NAME" ]; then
+		echo " * Installing $NAME binary..."
 		cd /tmp;
 		wget "$SERF_BIN_URL";
 		unzip $SERF_PKG_NAME -d $SERF_INSTALL_DIR;
 		cd -
-	}
+	else
+		echo " * $NAME binary found!"
+	fi
 }
 
 case "$1" in 
